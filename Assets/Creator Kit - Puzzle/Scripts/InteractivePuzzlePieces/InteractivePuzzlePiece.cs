18312f67275d2f3472d87c1d5a2a963dcc62ce21
Marwan Yahia <marwan.yahia.dev@gmail.com>
2026-08-08 23:49:41 +0300

    Added unity puzzles Assets

---
+++ Assets/Creator Kit - Puzzle/Scripts/InteractivePuzzlePieces/InteractivePuzzlePiece.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public abstract class InteractivePuzzlePiece<TComponent> : BaseInteractivePuzzlePiece
+where TComponent : Component
+{
+    public TComponent physicsComponent;
+}
+
+
+public abstract class BaseInteractivePuzzlePiece : MonoBehaviour
+{
+    public InputAction interactAction;
+    public Rigidbody rb;
+    public AudioClip activateSound;
+    public AudioClip deactivateSound;
+    public AudioSource puzzleAudioSource;
+
+    bool m_IsControlable;
+
+    private void OnEnable()
+    {
+        interactAction.Enable();
+        interactAction.started += OnInteractStarted;
+        interactAction.canceled += OnInteractCanceled;
+    }
+
+    private void OnDisable()
+    {
+        interactAction.started -= OnInteractStarted;
+        interactAction.canceled -= OnInteractCanceled;
+        interactAction.Disable();
+    }
+
+    protected void FixedUpdate()
+    {
+        if (interactAction.IsPressed() && m_IsControlable)
+        {
+            ApplyActiveState();
+        }
+        else
+        {
+            ApplyInactiveState();
+        }
+    }
+
+    private void OnInteractStarted(InputAction.CallbackContext ctx)
+    {
+        if (activateSound != null)
+        {
+            puzzleAudioSource.pitch = Random.Range(0.8f, 1.2f);
+            puzzleAudioSource.PlayOneShot(activateSound);
+        }
+    }
+
+    private void OnInteractCanceled(InputAction.CallbackContext ctx)
+    {
+        if (deactivateSound != null)
+        {
+            puzzleAudioSource.pitch = Random.Range(0.8f, 1.2f);
+            puzzleAudioSource.PlayOneShot(deactivateSound);
+        }
+    }
+    
+    protected abstract void ApplyActiveState ();
+
+    protected abstract void ApplyInactiveState ();
+
+    public void EnableControl ()
+    {
+        m_IsControlable = true;
+    }
+}
